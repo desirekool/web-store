e3e5f12bcd307db10dfabac061a8fb0b62858e54
Ahmed Bilal <ahmed.bilal@t-online.de>
2024-01-26 08:19:02 +0100

    moved toaster css to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
